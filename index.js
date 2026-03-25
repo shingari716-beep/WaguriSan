f97b556045f5f73f84b16aca930d8b36f19871fa
shingari716-beep <shingari716@gmail.com>
2026-03-25 11:23:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+client.on('ready', () => {
+  console.log('Bot is online');
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('pong');
+  }
 });
 
 client.login(process.env.TOKEN);
